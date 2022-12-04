1496637001f97e66bb1c50e38c7f1ddaa97b3a3b
Younis <younis.raafat2@gmail.com>
2022-12-04 20:15:16 +0100

    Modified let_it_snow.py

--- let_it_snow.py
+++ let_it_snow.py
@@ -1,6 +1,6 @@
 import turtle
 import numpy as np
-
+from random import randint
 
 def main(speed=0, bg_color="grey"):
     # create Turtle object
@@ -14,7 +14,7 @@ def main(speed=0, bg_color="grey"):
   
  
     """TODO: define different colors here"""
-
+    color = ["white", "green", "blue", "orange", "red", "purple","cyan","black","brown","yellow"]
 
     for _ in range(10):
         # define some params
@@ -22,8 +22,8 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
-
-        # Go to the start position of the snowflake
+        myTurtle.pencolor(color[_ % 10])
+        # Go to the start position of the snowflakepytho
         myTurtle.penup()
         myTurtle.goto(pos[0], pos[1])
         myTurtle.pendown()
